90c6150cfbd2d95f39712d9d065cbd91fa3ed9a3
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-04-07 19:25:43 -0400

    Create a class library project

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿//namespace StringLibrary
+//{
+//	public class Class1
+//	{
+
+//	}
+//}
+namespace UtilityLibraries;
+
+public static class StringLibrary
+{
+	public static bool StartsWithUpper(this string? str)
+	{
+		if (string.IsNullOrWhiteSpace(str))
+			return false;
+
+		char ch = str[0];
+		return char.IsUpper(ch);
+	}
+}
